7e57ba52e3a0ef676bdff1529f1cf0d6cca15005
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-06-10 12:14:33 -0400

    setting up creating account and signing in

---
+++ BarMate/BarMate/BarSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BarMate
+//
+//  Created by Jorge Catalan on 6/10/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+import UIKit
+
+class BarSignUpViewController: UIViewController {
+    @IBOutlet weak var EmailTextField: UITextField!
+    @IBOutlet weak var PasswordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func createAccountButton(sender: AnyObject) {
+        
+    }
+
+}
+
---
+++ BarMate/BarMate/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  BarMate
+//
+//  Created by Jorge Catalan on 6/10/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
